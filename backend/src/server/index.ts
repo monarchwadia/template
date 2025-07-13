383e171c8fcdf392182c7c42677ccb4b15c7423f
Monarch Wadia <monarchwadia@gmail.com>
2025-07-12 21:42:32 -0400

    moved to express

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -1,18 +1,22 @@
 // import { db } from "./db";
 import { buildRootRouter } from "../routes/buildRootRouter";
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import express from 'express';
+import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import { createContext } from './context';
 
 const rootRouter = buildRootRouter();
 
 export type AppRouter = typeof rootRouter;
 
-export const buildServer = () => {   
-  const server = createHTTPServer({
-    router: rootRouter,
-    createContext
-  });
-
-  return server;
+export const buildServer = () => {
+  const app = express();
+  app.use(
+    '/trpc',
+    createExpressMiddleware({
+      router: rootRouter,
+      createContext,
+    })
+  );
+  return app;
 }
 
